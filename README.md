ef717971659443dc2291055bebd400460ee47da2
shivamaiuse2 <shivamaiuse2@gmail.com>
2025-10-07 13:03:51 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mygate_coepd
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/mygate_coepd/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mygate_coepd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
